c4a388d87e3a1bac71ea32418cb686a117f5359e
dungnguyenit <dungnguyenze@gmail.com>
2022-03-30 15:23:14 +0700

    them

--- src/components/ Invest/ Invest.js
+++ src/components/ Invest/ Invest.js
@@ -1,13 +1,66 @@
-import React from 'react';
-import './ invest.scss'
+import React from "react";
+import "./ invest.scss";
+import { DropdownButton, Dropdown } from "react-bootstrap";
 
-const  Invest = () => {
+const Invest = () => {
   return (
-    <div className='invest'> 
-            <img src={require('../assets/Frame 758.png')} alt="logo" className='invest-logo'/>
-
+    <div className="invest">
+      <h2>Đầu tư UNIVEST với 3 bước đơn giản</h2>
+      <div className="invest-item">
+        <div className="invest-dropdown">
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Tải App và đăng ký tài khoản"
+          >
+            <Dropdown.Item href="">
+              Nhận lãi hấp dẫn lên tới 6%
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              Miễn phí nạp và rút tiền
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-1">
+              Phát hành bởi các ngân hàng và quy định tài chính
+            </Dropdown.Item>
+          </DropdownButton>
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Dropdown Lựa chọn sản phẩm đầu tư"
+          >
+            <Dropdown.Item href="#/action-2">
+              Nhận lãi hấp dẫn lên tới 6%
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              Miễn phí nạp và rút tiền
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-1">
+              Phát hành bởi các ngân hàng và quy định tài chính
+            </Dropdown.Item>
+          </DropdownButton>
+          <DropdownButton
+            id="dropdown-basic-button"
+            title="Theo dõi tăng trưởng tài sản từng ngày"
+          >
+            <Dropdown.Item href="#/action-2">
+              Nhận lãi hấp dẫn lên tới 6%
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-3">
+              Miễn phí nạp và rút tiền
+            </Dropdown.Item>
+            <Dropdown.Item href="#/action-1">
+              Phát hành bởi các ngân hàng và quy định tài chính
+            </Dropdown.Item>
+          </DropdownButton>
+        </div>
+        <div>
+          <img
+            src={require("../assets/Rectangle 205.png")}
+            alt="logo"
+            className="invest-logo"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default  Invest
+export default Invest;
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -4,9 +4,27 @@ import './product.scss'
 const Product = () => {
   return (
     <div className="product">
-      <img src={require('../assets/Frame 757.png')} 
+     
+      <h2>Sản phẩm của UNIVEST</h2>
+      <button className='product-btn1'>Tích luỹ</button>
+      <div className='product-item'>
+        <div className='product-text'>
+          <h4>Tích luỹ linh hoạt, lãi nhận hàng ngày</h4>
+          <p>Nhận lãi hấp dẫn lên tới 8%/năm</p>
+          <p>Miễn phí nạp và rút tiền</p>
+          <p>Phát hành bởi các ngân hàng và quy định tài chính</p>
+          <div className='product-btn'>
+          <button className='product-btn1'>Tìm hiểm thêm </button>
+          </div>
+          
+          
+        </div>
+        <div className='product-icon'>
+        <img src={require('../assets/product-icon.png')} 
       alt="logo" 
       className='produc-logo'/>
+        </div>
+      </div>
     </div>
   )
 }
